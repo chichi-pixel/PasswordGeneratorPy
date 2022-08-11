b2ffd28ef6fdb244d6a2e77a6dea5a4353be9f22
chichi-pixel <hatice.peucker@hotmail.com>
2022-08-12 01:40:21 +0200

    generate your password in py!

---
+++ PasswordGenerator.py
@@ -0,0 +1,15 @@
+import random 
+
+print("PasswordGenerator by Htc")
+
+chars = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!£$%&^*@?'
+num_char = len(input("how long should be your password?"))
+new_num_char = int(num_char)
+print("Your password has" + new_num_char + "characters.")
+
+password = ''
+for i in range(num_char):
+    password += random.choice(chars)
+
+print(password)
+print("Your password is written")
